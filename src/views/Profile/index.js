948290e2b9e87b36b49220c49dece646e7c92814
AnchorDev <01.jakub.kotwica@gmail.com>
2023-12-18 23:45:07 +0100

    Update index.js
    dodanie zdjec

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -10,41 +10,102 @@ export function Profile({ navigation }) {
 
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.profileText}>Maruch12</Text>
-            <View style={styles.profileInfo}>
-                <View style={styles.profileBackground}>
+    <View style={styles.container}>
+        <Text style={styles.profileText}>Maruch12</Text>
+        <View style={styles.profileInfo}>
+            <View style={styles.profileBackground}>
                 <Image
                     source={require("../../img/profilowe.jpg")}
                     style={styles.profileImage}
+                />
+            </View>
+            <View style={styles.statsContainer}>
+                <View style={styles.statItem}>
+                    <Text style={styles.statNumber}>102</Text>
+                    <Text style={styles.statLabel}>Obserwujący</Text>
+                </View>
+                <View style={styles.statItem}>
+                    <Text style={styles.statNumber}>520</Text>
+                    <Text style={styles.statLabel}>Polubienia</Text>
+                </View>
+                <View style={styles.statItem}>
+                    <Text style={styles.statNumber}>52</Text>
+                    <Text style={styles.statLabel}>Obserwuje</Text>
+                </View>
+            </View>
+        </View>
+        <View style={styles.userInfo}>
+            <Text style={styles.userName}>Marek Marucha</Text>
+            <Text style={styles.location}>🏡 Kielce</Text>
+            <Text style={styles.description}>
+                Człowiek wielu talentów, lecz sekretem jest ich poznanie. F1 fan,
+                zwłaszcza oponiarza.
+            </Text>
+            <View style={styles.userPhotosContainer}>
+                <View style={styles.userPhotosRow}>
+                    <Image
+                        source={require("../../img/profilowe.jpg")}
+                        style={styles.userPhotos}
+                    />
+                    <Image
+                        source={require("../../img/prof1.jpg")}
+                        style={styles.userPhotos}
+                    />
+                    <Image
+                        source={require("../../img/prof2.jpg")}
+                        style={styles.userPhotos}
                     />
                 </View>
-                <View style={styles.statsContainer}>
-                    <View style={styles.statItem}>
-                        <Text style={styles.statNumber}>102</Text>
-                        <Text style={styles.statLabel}>Obserwujący</Text>
-                    </View>
-                    <View style={styles.statItem}>
-                        <Text style={styles.statNumber}>520</Text>
-                        <Text style={styles.statLabel}>Polubienia</Text>
-                    </View>
-                    <View style={styles.statItem}>
-                        <Text style={styles.statNumber}>52</Text>
-                        <Text style={styles.statLabel}>Obserwuje</Text>
-                    </View>
+                <View style={styles.userPhotosRow}>
+                    <Image
+                        source={require("../../img/prof3.jpg")}
+                        style={styles.userPhotos}
+                    />
+                    <Image
+                        source={require("../../img/profilowe.jpg")}
+                        style={styles.userPhotos}
+                    />
+                    <Image
+                        source={require("../../img/prof1.jpg")}
+                        style={styles.userPhotos}
+                    />
+                </View>
+                <View style={styles.userPhotosRow}>
+                    <Image
+                        source={require("../../img/profilowe.jpg")}
+                        style={styles.userPhotos}
+                    />
+                    <Image
+                        source={require("../../img/prof3.jpg")}
+                        style={styles.userPhotos}
+                    />
+                    <Image
+                        source={require("../../img/prof2.jpg")}
+                        style={styles.userPhotos}
+                    />
+                </View>
+                <View style={styles.userPhotosRow}>
+                    <Image
+                        source={require("../../img/profilowe.jpg")}
+                        style={styles.userPhotos}
+                    />
+                    <Image
+                        source={require("../../img/prof1.jpg")}
+                        style={styles.userPhotos}
+                    />
+                    <Image
+                        source={require("../../img/profilowe.jpg")}
+                        style={styles.userPhotos}
+                    />
                 </View>
             </View>
-            <View style={styles.userInfo}>
-                <Text style={styles.userName}>Marek Marucha</Text>
-                <Text style={styles.location}>🏡 Kielce</Text>
-                <Text style={styles.description}>Człowiek wielu talentów, lecz sekretem jest ich poznanie. F1 fan, zwłaszcza oponiarza.</Text>
-            </View>
-            <TouchableOpacity onPress={openOptions} style={styles.optionsButton}>
-                <Image
-                    source={require("../../img/kolo.png")}
-                    style={styles.optionsIcon}
-                />
-            </TouchableOpacity>
         </View>
+        <TouchableOpacity onPress={openOptions} style={styles.optionsButton}>
+            <Image
+                source={require("../../img/kolo.png")}
+                style={styles.optionsIcon}
+            />
+        </TouchableOpacity>
+    </View>
     );
 }
